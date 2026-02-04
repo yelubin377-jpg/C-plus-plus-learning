98ba6cc87d75556ca3c01446cb941431840fa579
Michael _377 <yelubin377@gmail.com>
2026-02-04 18:58:41 +0800

    Update 引用与指针.cpp

--- 引用与指针.cpp
+++ 引用与指针.cpp
@@ -1,28 +1,27 @@
-// YinYong Yu ZhiZhen DuiBi ShiYan
 // 引用与指针对比实验
 
 #include <iostream>
 using namespace std;
 
-// ZhiChuanDi 值传递
+// 值传递
 void ZhiChuanDi(int ShuZi)  // 数字
 {
     ShuZi = 100;
 }
 
-// ZhiZhenChuanDi 指针传递
+//  指针传递
 void ZhiZhenChuanDi(int *ZhiZhen)  // 指针
 {
     *ZhiZhen = 200;
 }
 
-// YinYongChuanDi 引用传递
+//  引用传递
 void YinYongChuanDi(int &YinYong)  // 引用
 {
     YinYong = 300;
 }
 
-// FanHui YinYong 返回引用
+//  返回引用
 int ShuZu[5] = {1, 2, 3, 4, 5};  // 数组
 int& HuoQuYuanSu(int SuoYin)    // 获取元素(索引)
 {
@@ -31,7 +30,7 @@ int& HuoQuYuanSu(int SuoYin)    // 获取元素(索引)
 
 int main()
 {
-    // JiBen YongFa 基本用法
+    // 基本用法
     int BianLiang = 10;           // 变量
     int *ZhiZhen = &BianLiang;    // 指针
     int &YinYong = BianLiang;     // 引用
@@ -40,13 +39,13 @@ int main()
     cout << "ZhiZhen FangWen: " << *ZhiZhen << endl;       // 指针访问
     cout << "YinYong FangWen: " << YinYong << endl;        // 引用访问
     
-    // XiuGai CeShi 修改测试
+    // 修改测试
     *ZhiZhen = 50;
     cout << "TongGuo ZhiZhen XiuGaiHou: " << BianLiang << endl;  // 通过指针修改后
     YinYong = 80;
     cout << "TongGuo YinYong XiuGaiHou: " << BianLiang << endl;  // 通过引用修改后
     
-    // CanShu ChuanDi DuiBi 参数传递对比
+    //  参数传递对比
     cout << "\nCanShu ChuanDi CeShi:" << endl;  // 参数传递测试
     int A = 1;
     ZhiChuanDi(A);
@@ -58,17 +57,17 @@ int main()
     YinYongChuanDi(A);
     cout << "YinYongChuanDi Hou: " << A << endl;  // 引用传递后
     
-    // YinYong FanHuiZhi Dang ZuoZhi 引用返回值当左值
+    //  引用返回值当左值
     cout << "\nYinYong FanHuiZhi:" << endl;  // 引用返回值
     cout << "ShuZu[2] = " << ShuZu[2] << endl;  // 数组
     HuoQuYuanSu(2) = 999;
     cout << "XiuGaiHou ShuZu[2] = " << ShuZu[2] << endl;  // 修改后数组
     
-    // YinYong BuNeng ChongXin BangDing 引用不能重新绑定
+    // 引用不能重新绑定
     cout << "\nChongBangDing CeShi:" << endl;  // 重绑定测试
     int M = 111, N = 222;
     int &R = M;
-    R = N;  // ZheShi FuZhi, BuShi ChongBangDing 这是赋值,不是重绑定
+    R = N;  // 这是赋值,不是重绑定
     cout << "M = " << M << ", N = " << N << endl;
     
     return 0;
